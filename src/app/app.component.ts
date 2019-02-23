6e8380b4c0e37dc77ca9adc912b65c9ee3e5eb3e
Mauricio Siller <mauriciosiller@gmail.com>
2019-02-23 14:59:41 -0600

    add mapbox component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,12 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  styles: [`
+    mgl-map {
+      height: 100%;
+      width: 100%;
+    }
+  `]
 })
 export class AppComponent {
   title = 'amulet-ui';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxMapboxGLModule.withConfig({
+      accessToken: 'pk.eyJ1IjoibXNpbGxlciIsImEiOiJjanNoeHJuMmYxaGJzNDRxZ3h1NWJrNXUxIn0.HzGVd3cMbsSqszrcCM8yTA' // Optionnal, can also be set per map (accessToken input of mgl-map)
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
